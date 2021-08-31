054af382728166cf1cb34b2279ebe76ff72e828b
cg99 <darkinfinitespace@gmail.com>
2021-08-31 11:26:13 +0545

    fix merge

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -1,11 +1,7 @@
 <div class="header">
     <div class="logo">
         <a href="<?php echo esc_url(site_url()); ?>">
-<<<<<<< HEAD
             <img src="<?php echo esc_url(get_theme_mod('logo', 'default')); ?>" alt="logo">
-=======
-            <img src="<?php echo esc_url(get_theme_mod('logo', 'default')); ?>">
->>>>>>> 358d1ebe91cbcb85146803d2412771d2d7062852
         </a>
         <a class="logo-title hidden">Mayur</a>
     </div>
@@ -13,13 +9,8 @@
     <div class="wrapper" style="margin-left: auto">
         <div class="header-ad">
             <figure style="margin: 0;">
-<<<<<<< HEAD
                 <img src="<?php echo esc_url(get_theme_mod('header', 'default')); ?>" alt="banner">
                 <figcaption class="hidden">Header</figcaption>
-=======
-                <img src="<?php echo esc_url(get_theme_mod('header', 'default')); ?>">
-                <figcaption class="hidden">Header</figcaption>     
->>>>>>> 358d1ebe91cbcb85146803d2412771d2d7062852
             </figure>
         </div>
     </div>
